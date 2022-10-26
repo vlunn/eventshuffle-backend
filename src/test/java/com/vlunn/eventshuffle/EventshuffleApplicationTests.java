ce67aaa0b15cfb421ba81b07130dc8f9f950ada7
Vivian Lunnikivi <vlunnikivi@gmail.com>
2022-10-26 16:34:29 +0300

    Initial project

---
+++ src/test/java/com/vlunn/eventshuffle/EventshuffleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlunn.eventshuffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventshuffleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
