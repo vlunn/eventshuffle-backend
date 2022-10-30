f1b9cdc4946541847bfa730709ca0310aa221778
Vivian Lunnikivi <vlunnikivi@gmail.com>
2022-10-30 17:45:05 +0200

    Implement REST API fetch for all happenings.

--- src/main/java/com/vlunn/eventshuffle/api/controller/HappeningController.java
+++ src/main/java/com/vlunn/eventshuffle/api/controller/HappeningController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vlunn.eventshuffle.api.model.HappeningDTO;
+import com.vlunn.eventshuffle.api.model.mapper.HappeningModelMapper;
+import com.vlunn.eventshuffle.business.service.SchedulingService;
 import com.vlunn.eventshuffle.exception.NotImplementedException;
 
 import lombok.AllArgsConstructor;
@@ -19,22 +22,31 @@ import lombok.AllArgsConstructor;
  * Defines the API for handling happenings.
  */
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/events")
 @AllArgsConstructor
 public class HappeningController {
 
     private static final Logger logger = 
         LoggerFactory.getLogger(HappeningController.class);
 
-    @GetMapping
+    @Autowired
+    private SchedulingService schedulingService;
+
+    @Autowired
+    private HappeningModelMapper mapper;
+
+    @GetMapping()
     public List<HappeningDTO> getHappenings() {
-        logger.debug("Get happenings.");
-        throw new NotImplementedException("NOT_IMPLEMENTED_MSG");
+        logger.debug("Received API call: getHappenings.");
+
+        return schedulingService.getHappenings().stream()
+            .map(h -> mapper.toDTO(h))
+            .toList();
     }
 
     @PutMapping()
     public HappeningDTO createHappening(@RequestBody final HappeningDTO happening) {
-        logger.debug("Create happening: {}", happening);
+        logger.debug("Received API call: createHappening with body: {}", happening);
         final String msg = "API endpoint for creating happenings is not yet implemented.";
         throw new NotImplementedException(msg);
     }
